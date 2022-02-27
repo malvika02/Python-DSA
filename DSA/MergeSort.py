64b4c744ed17bf14de6d0f9bcdb5ac221bf0f057
Malvika Verma <malvika2ndmar2000@gmail.com>
2022-02-27 19:57:52 +0530

    Implementing

---
+++ DSA/MergeSort.py
@@ -0,0 +1,9 @@
+def MergeSort(arr):
+    for i in range(len(arr)):
+        m = len(arr)/2
+        L = arr[:m]
+        R = arr[m+1:]
+
+    return m
+m = [12,32,43,41,33,1,9]
+print(m)
--- DSA/insertionSort.py
+++ DSA/insertionSort.py
@@ -1,2 +1,21 @@
-arr = [1,2,3,4,5,6,7,8]
-print(len(arr))
+import random
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+            key = arr[i]
+            
+            for j in range(i-1, 0, -1):
+                if arr[i] < arr[j]:
+                    key = arr[i]
+                    arr[i] = arr[j]
+                    arr[j] = key
+                    
+                else:
+                    key = arr[i]
+                    
+    return arr
+
+array = [83, 45, 4, 12, 32, 69]
+# array = [random.randint(-2,100) for i in range(10)]
+print(insertionSort(array))
+
+            
