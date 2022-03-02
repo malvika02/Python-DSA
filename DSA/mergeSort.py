e8861621d3d6985071eea2d0e427c081c9a5ae8e
Malvika Verma <malvika2ndmar2000@gmail.com>
2022-03-02 21:06:21 +0530

    modification

--- DSA/mergeSort.py
+++ DSA/mergeSort.py
@@ -1,12 +1,46 @@
-def MergeSort(arr):
-    for i in range(len(arr)):
-        m = len(arr)/2
-        L = arr[:m]
-        R = arr[m+1:]
+def mergeSort(A, B):
+    (m,n) = (len(A),len(B))
+    (C,i,j,k) = 
 
-    return m
-m = [12,32,43,41,33,1,9]
-print(m)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def MergeSort(arr):
+#     for i in range(len(arr)):
+#         m = len(arr)/2
+#         L = arr[:m]
+#         R = arr[m+1:]
+
+#     return m
+# m = [12,32,43,41,33,1,9]
+# print(m)
 
 
             
