531b6532ebc2ccfe8ce54d0cf49d888aacc9680d
Malvika Verma <malvika2ndmar2000@gmail.com>
2022-03-04 20:17:10 +0530

    Merge Sort

--- DSA/mergeSort.py
+++ DSA/mergeSort.py
@@ -1,43 +1,12 @@
 import random
-def merge(A, B):
-    #arr[A] & arr[B] are of length m,n respectively
-    (m,n) = (len(A),len(B)) 
-    (C,i,j,k) = ([], 0, 0, 0)
-    while k < m+n:
-        if i == m: #A is empty
-            C.extend(B[j:])
-            k = k + (n-j)
-        elif j == n:
-            C.extend(A[i:])
-            k = k + (n-i)
-        elif A[i] < B[j]:
-            C.append(A[i])
-            (i,k) = (i+1,k+1)
-        else:
-            C.append(B[j])
-            (j,k) = (j+1, k+1)
-        return(C)
-def mergeSort(A):
-    n = len(A)
-    if n <= 1:
-        return(A)
-    L = mergeSort(A[:n//2])
-    R = mergeSort(A[n//2:])
 
-    B = merge(L,R)
-    return(B)
-array = [random.randint(0,100) for i in range(10)]
-print(array)
-print(mergeSort(array))
-
-
-
-
-
-
-
-
-
+def mergeSort(arr):
+    # Store the length of the list
+    arrlength = len(arr)
+    # list with length less than is already sorted
+    if arrlength == 1:
+        return arr
+    # identify the list midpoint and partition the list into a left partition 
 
 
 
