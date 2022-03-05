04bb7304c258e7a83c138dc617860f9456849873
Malvika Verma <malvika2ndmar2000@gmail.com>
2022-03-05 22:01:42 +0530

    Quick Sort

--- DSA/quickSort.py
+++ DSA/quickSort.py
@@ -1,3 +1,12 @@
-def quickSort():
-    for i in range(arr, low, high):
-        if()
+# QuickSort [C.A.R Hoare]
+import random
+def quickSort(arr, left, right):
+    if left < right:
+        partiton_pos = partition(arr, left, right)
+        # elements that are less than the pivot element
+        quickSort(arr, left, partiton_pos-1)    
+        # elements that are greater than the pivot element
+        quickSort(arr, partiton_pos+1, right)
+
+def partition(arr, left, right):
+
