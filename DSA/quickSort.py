524475031d22582ee1352cb9ae4cd6cc783b381e
Malvika Verma <malvika2ndmar2000@gmail.com>
2022-03-05 22:15:31 +0530

    Quick Sort:modification in main function

--- DSA/quickSort.py
+++ DSA/quickSort.py
@@ -12,4 +12,28 @@ def quickSort(arr, left, right):
 # returns the index of pivot element after the first step of quick sort.
 def partition(arr, left, right):
     i = left
+    j = right-1
+    pivot = arr[right]
+
+    while i < j:
+        while i < right and arr[i] < pivot:
+            i += 1
+        while j > left and arr[j] >= pivot:
+            j -= 1
+    if i < j:
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+    if arr[i] > pivot:
+        temp = arr[i]
+        arr[i] = arr[right]
+        arr[right] = temp
+    return i
+
+array = [random.randint(-2,100) for i in range(10)]
+
+quickSort(array, 0, len(array)-1)
+print(array)
+
+
 
