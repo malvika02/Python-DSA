30ef6cff35221c9d65942c779915ed8bfee3c659
Malvika Verma <malvika2ndmar2000@gmail.com>
2022-03-06 22:17:57 +0530

    Creating a LL with a single head node

--- DSA/linkedList.py
+++ DSA/linkedList.py
@@ -1,7 +1,11 @@
 class node:
-    def __init__(self, data):
+    def __init__(self, data, next=None):
         self.data = data
-        self.next = None
+        self.next = next
+# A linked list class with a single head node
 class linkedList:
     def __init__ (self):
         self.head = None
+# creating a single node 
+first = node(3)
+print(first.data)
