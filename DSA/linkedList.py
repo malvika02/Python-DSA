85b6e4114560d4ed2d279c87cd13ea46caac494c
Malvika Verma <malvika2ndmar2000@gmail.com>
2022-03-07 20:51:15 +0530

    Inserting elements in Linked List

--- DSA/linkedList.py
+++ DSA/linkedList.py
@@ -1,11 +1,25 @@
-class node:
-    def __init__(self, data, next=None):
+class Node:
+    def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
 # A linked list class with a single head node
 class linkedList:
     def __init__ (self):
         self.head = None
-# creating a single node 
-first = node(3)
-print(first.data)
+    
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+    
+    def print(self):
+        if self.head is None:
+            print("Linked List is empty")
+            return
+        # temperoray variable to iterate the elements in the linked list one by one
+        itr = self.head
+        llstr = ""
+        while itr:
+            llstr += str(itr.data) + '--->'
+            itr = itr.next
+
+        print(llstr)
