e4a7830e98587d193c095e83477a4beabdaf51d5
Malvika Verma <malvika2ndmar2000@gmail.com>
2022-03-17 22:06:55 +0530

    Trying Hash table

--- DSA/hashTable.py
+++ DSA/hashTable.py
@@ -1,33 +1,29 @@
-# hash function
-# def get_hash(key):
-#     h = 0
-#     for char in key:
-#         # ord gives the ASCII value
-#         h += ord(char)
-#         # assume size of the list is 100
-#     return h % 100
-# print(get_hash('march 28'))
-
 class HashTable:
     # list comprehension offers a shorter syntax
     # newlist = [expression for item in iterable if condition == True]
     def __init__(self):
         self.max = 100
-        self.arr =[None for i in range(self.max)]
+        self.arr =[[] for i in range(self.max)]
+        # print(self.arr)
     def get_hash(self, key):
         h = 0
         for char in key:
             h += ord(char)
         return h % self.max
-    # implement function to add and get an item from the hash table
-    # key value pair
+    #  add function to add an item to the hash table
+    #  key value pair
     def add(self, key, val):
+        # index value 
         h = self.get_hash(key)
-        self.arr[h] = val
+        value = [key, value]
+
+        if 
+        
     
-    def get(self, key):
-        h = self.get_hash(key)
-        return self.arr[h]
+    # def get(self, key):
+    #     h = self.get_hash(key)
+    #     return self.arr[h]
+h = HashTable()
 
 
 
